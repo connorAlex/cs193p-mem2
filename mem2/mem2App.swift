f422b230a9586a10b9a448a5f9ff6baa40a27421
connorAlex <alex.connor.cochrane@gmail.com>
2024-07-01 11:34:02 -0500

    Initial Commit

---
+++ mem2/mem2App.swift
@@ -0,0 +1,17 @@
+//
+//  mem2App.swift
+//  mem2
+//
+//  Created by Connor Cochrane on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct mem2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
