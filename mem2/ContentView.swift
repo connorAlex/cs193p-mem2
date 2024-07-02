979461cce2d0d742ff86e39dcbadbcd1baf22800
connorAlex <alex.connor.cochrane@gmail.com>
2024-07-02 11:25:49 -0500

    ScrollView card refactor Opacity

--- mem2/ContentView.swift
+++ mem2/ContentView.swift
@@ -14,35 +14,50 @@ struct ContentView: View {
     
     var body: some View {
         VStack{
-            HStack {
-                ForEach(0..<cardCount, id: \.self) { index in
-                    CardView(content: emojis[index])
-                }
+            ScrollView {
+                cards
             }
-            .foregroundColor(.orange)
-            
-            HStack {
-                Button(action: {
-                    if cardCount > 1 {
-                        cardCount -= 1
-                    }
-                    
-                }, label: {
-                    Image(systemName: "rectangle.stack.fill.badge.minus")
-                })
-                Spacer()
-                Button(action: {
-                    if cardCount < emojis.count {
-                        cardCount += 1
-                    }
-                }, label: {
-                    Image(systemName: "rectangle.stack.fill.badge.plus")
-                })
-            }
-            .imageScale(.large)
-            .font(.largeTitle)
+            Spacer()
+            cardCountAdjusters
         }
         .padding()
+    }
+    var cardCountAdjusters: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120)), ]) {
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+        }
+        .foregroundColor(.orange)
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+            
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardRemover: some View {
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.fill.badge.minus")
+    }
+    
+    var cardAdder: some View {
+        cardCountAdjuster(by: 1, symbol: "rectangle.stack.fill.badge.plus")
     }
     
     struct CardView: View {
@@ -54,13 +69,14 @@ struct ContentView: View {
         var body: some View {
             ZStack {
                 let base = RoundedRectangle(cornerRadius: 12)
-                if isFacedUp {
+                Group {
                     base.fill(.white)
                     base.strokeBorder(lineWidth: 2)
                     Text(content).font(.largeTitle)
-                } else {
-                    base
                 }
+                .opacity(isFacedUp ? 1 : 0)
+                base.fill().opacity(isFacedUp ? 0 : 1)
+                
             }
             .onTapGesture {
                 isFacedUp.toggle()
